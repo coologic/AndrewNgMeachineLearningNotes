e01dc254d158751adb9d19775fb95daf62819a5e
liang.zhou <liang.zhou@thoughtworks.com>
2019-12-27 09:06:33 +0800

    fix: add all answer

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AndrewNgMeachineLearningNotes
 吴恩达机器学习笔记、作业、答案
+
+## 作业.md 为完整答案，收集自网络
---
+++ answer/ex2/L1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+if __name__ == "__main__":
+    m = np.asmatrix([[1, 2], [3, 4]])
+    print(m)
+    print(np.multiply(m, m))
+    print(np.add(m, m))
